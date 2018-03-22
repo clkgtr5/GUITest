a4da128e0ca19f0947f34a5ec40d1776e7c0a223
JX <clkgtr5@163.com>
2018-03-22 15:04:57 -0500

    Initial commit

---
+++ src/MyGUITest.java
@@ -0,0 +1,16 @@
+import java.awt.Frame;
+
+
+public class MyGUITest extends Frame{
+
+    public MyGUITest(){
+
+    }
+
+    public static void main(String[] args){
+
+        MyGUITest mGUI = new MyGUITest();
+        
+    }
+
+}
